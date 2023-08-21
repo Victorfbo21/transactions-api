1fea058dcab90e0684a7a7690086d28576836059
Victor Bentes <victorfbo@outlook.com>
2023-08-21 19:58:26 -0300

    initial commit

---
+++ src/Config/dbConfig.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const dbConnect = () => {
+    let MONGO_URL = `mongodb+srv://${process.env.DATABASE_URL}:`
+    // MONGO_URL += `${process.env.DATABASE_PORT}`
+    // MONGO_URL += `/${process.env.DATABASE_DB}`
+    mongoose.set('strictQuery', false)
+    return mongoose.connect(MONGO_URL,
+        {
+            authSource: 'admin',
+            user: process.env.DATABASE_USER,
+            pass: process.env.DATABASE_PASSWORD,
+            dbName: process.env.DATABASE_DB
+        }
+    ).then(
+        () => {
+            console.log('Connected to', MONGO_URL)
+            console.log(process.env.DATABASE_URL)
+            return true
+        }
+    ).catch(
+        (e) => {
+
+            console.log('DB connection error:', e)
+            throw e
+        }
+    )
+}
+
+export default dbConnect
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+import cors from 'cors'
+import { config } from 'dotenv'
+import dbConnect from './Config/dbConfig.js'
+import { Router, Request, Response } from 'express';
+
+config({
+    path: '.env'
+})
+const app = express();
+const port = process.env.PORT
+const route = Router()
+
+app.use(express.json())
+app.use(cors())
+route.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'hello world with Typescript' })
+})
+
+app.use(route)
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    next()
+})
+
+dbConnect().then(() => {
+    app.listen(port, () => {
+        console.log(`Ouvindo em http://localhost:${port}`)
+    })
+}).catch(err => console.error('Error on db connect', err))
+
