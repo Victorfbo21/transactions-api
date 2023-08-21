719736931e1910994b83911c076dd16225f1ac45
Victor Bentes <victorfbo@outlook.com>
2023-08-21 20:26:56 -0300

    add User Schema and Store Schema

---
+++ src/Schemas/storeSchema.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const StoreSchema = new mongoose.Schema({
+    storeName: { type: String },
+    storeEmail: { type: String },
+    storePassword: { type: String },
+    storeDocument: { type: Number, unique: true },
+}, {
+    timestamps: true
+})
+
+const Store = mongoose.model('Store', StoreSchema)
+
+export default Store
+
---
+++ src/Schemas/usersSchema.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+    userName: { type: String },
+    userEmail: { type: String },
+    userPassword: { type: String },
+    userDocument: { type: Number, unique: true },
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User
+
